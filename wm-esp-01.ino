9680ec129979c776b9dd730b83b282196d835f8e
Sajeesh Kiliyani <skiliyani@binaryfountain.com>
2021-11-22 00:42:55 +0530

    Implemented OTA and non-blocking sensor reading

--- wm-esp-01.ino
+++ wm-esp-01.ino
@@ -1,6 +1,10 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <NewPing.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#include <NoDelay.h>
 
 #define HCSR04_PIN_TRIG 2
 #define HCSR04_PIN_ECHO 0
@@ -12,6 +16,7 @@ const char* mqtt_server = "192.168.8.10"; //pi4
 WiFiClient espClient;
 PubSubClient mqttClient(espClient);
 NewPing hcsr04(HCSR04_PIN_TRIG,HCSR04_PIN_ECHO);
+noDelay hcsr04Time(1000);
 
 char msg[50];
 
@@ -20,20 +25,55 @@ void setup_wifi() {
   delay(10);
   // We start by connecting to a WiFi network
   Serial.println();
-  Serial.print("Connecting to ");
+  Serial.println("Booting");
   Serial.println(ssid);
-
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
   }
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_FS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+
+  ArduinoOTA.begin();
 
   randomSeed(micros());
 
   Serial.println("");
-  Serial.println("WiFi connected");
+  Serial.println("Ready");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 }
@@ -52,6 +92,9 @@ void disconnect_wifi() {
 void reconnect() {
   // Loop until we're reconnected
   while (!mqttClient.connected()) {
+
+    ArduinoOTA.handle();
+    
     Serial.print("Attempting MQTT connection...");
     // Create a random client ID
     String clientId = "ESP8266Client-";
@@ -63,8 +106,8 @@ void reconnect() {
       Serial.print("failed, rc=");
       Serial.print(mqttClient.state());
       Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
+      // Wait 1 second before retrying
+      delay(1000);
     }
   }
 }
@@ -78,19 +121,22 @@ void setup() {
 
 void loop() {
 
+  ArduinoOTA.handle();
+
   if (!mqttClient.connected()) {
     reconnect();
   }
 
   mqttClient.loop();
-   
-  int hcsr04Dist = hcsr04.ping_cm();
-  delay(10);
-  snprintf (msg, 50, "%ld", hcsr04Dist);
-  Serial.print(F("Distance: ")); Serial.print(hcsr04Dist); Serial.println(F("[cm]"));
-  Serial.print("Publish message: ");
-  Serial.println(msg);
-  mqttClient.publish("home/terrace/tank/water/level", msg);
 
-  delay(60 * 1000); // one min
+  if(hcsr04Time.update()) {
+    int hcsr04Dist = hcsr04.ping_cm();
+    delay(10);
+    snprintf (msg, 50, "%ld", hcsr04Dist);
+    Serial.print(F("Distance: ")); Serial.print(hcsr04Dist); Serial.println(F("[cm]"));
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    mqttClient.publish("home/terrace/tank/water/level", msg);    
+  }
+
 }
